5bc72d768becf9e1abddff4f2a44212b2d4ddd78
ddecodes <decoder0509@gmail.com>
2024-11-15 12:25:03 +0530

    Update Contripage.py image

--- frontend/Contripage.py
+++ frontend/Contripage.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import time
 def render():
-    st.image("contribute image.webp")
+    st.image("frontend/contribute image.webp")
     st.title(":green[Support Us!]")
     st.write("___________________________________________________________________")
     st.write("We would love to hear your feedback and any audio contributions you might have! Please upload your audio file and provide a brief explanation of your feedback. Your input will help us improve and is greatly appreciated. Once you're ready, simply send your file and comments to us via email. Thank you for your support!")
